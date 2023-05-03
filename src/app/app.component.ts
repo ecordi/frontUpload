7e0a51273c900b1966a083cb09cc00c818a1158d
eugenio.cordi <eugenio.cordi@ross.com.ar>
2023-05-03 01:57:13 -0300

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'archivo';
+  fileToUpload: File | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any): void {
+    this.fileToUpload = event.target.files.item(0);
+  }
+
+  uploadFile(): void {
+    if (!this.fileToUpload) {
+      console.error('No se seleccionó ningún archivo');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', this.fileToUpload);
+
+    this.http.put('http://localhost:3000/api/v1/import-external-agents/create', formData)
+      .subscribe(
+        response => console.log('Solicitud completada con éxito', response),
+        error => console.error('Ocurrió un error al hacer la solicitud', error)
+      );
+  }
 }
